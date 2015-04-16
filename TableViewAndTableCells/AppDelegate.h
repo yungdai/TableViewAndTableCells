755a2e34d3b152b29cc73358bde0bd32b1607f2a
Yung Dai <yungchidai@gmail.com>
2015-04-16 14:01:29 -0400

    Initial Commit

---
+++ TableViewAndTableCells/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableViewAndTableCells
+//
+//  Created by Yung Dai on 2015-04-16.
+//  Copyright (c) 2015 Yung Dai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
